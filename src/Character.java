63cff07b85aa3a23e0aa82201f5116b5f419374e
MelissaAyllon <mayllon@espol.edu.ec>
2023-02-23 15:58:36 -0500

    Add the main classes to this project

---
+++ src/Character.java
@@ -0,0 +1,14 @@
+public class Character {
+    private String name;
+    private String urlImage;
+    /* aspects of the character for identifying */
+    private String colorHair;
+    private String team;
+    private String playPosition;
+    private boolean continuePlaying;
+
+
+
+    /* methods */
+
+}
---
+++ src/Round.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Round {
+    private String nameParticipant;
+    private String round;
+    private double points;
+    private String password; /* to log in for saving every game */
+    /* methods */
+    private ArrayList<Character> characters;
+
+
+
+
+
+
+}
